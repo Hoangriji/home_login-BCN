c798265f73e60b2c5c7b8d4fb4c180e72d1f19d5
Hoàng <164169035+Hoangriji@users.noreply.github.com>
2024-11-23 08:01:01 +0700

    Update home-login.js

--- assets/js/home-login.js
+++ assets/js/home-login.js
@@ -5,3 +5,6 @@ document.getElementById ("section1_wrapper_left_btn").addEventListener("click", 
       card.classList.remove("active");
     }, 250);
 });
+document.querySelector('head').innerHTML += `
+    <link rel="stylesheet" href="assets/css/style.css">
+`;
